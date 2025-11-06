7281a97cf60905035b31b6e6c11fd51495e4c4ae
koy6654 <koy410994@gmail.com>
2025-11-06 23:44:13 +0900

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,8 @@ const config: Config = {
         sm: 'calc(var(--radius) - 4px)',
       },
       fontFamily: {
+        sans: ['var(--font-geist-sans)', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
         lora: ['Lora', 'serif'],
       },
       keyframes: {
